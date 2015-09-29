1919f974b07cca3cb08e3d93fd41a3e56eb5bceb
Chad Evans <chad.evans@mendix.com>
2015-09-29 14:11:22 -0400

    Initial Commit

---
+++ src/ScrollThenFix/widget/ScrollThenFix.js
@@ -0,0 +1,122 @@
+/*
+    ScrollThenFix
+    ========================
+
+    @file      : ScrollThenFix.js
+    @version   : 0.1
+    @author    : Chad Evans
+    @date      : Tue, 29 Sep 2015 15:16:43 GMT
+    @copyright : 2015, Mendix B.v.
+    @license   : Apache v2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+    "ScrollThenFix/lib/jquery-1.11.2"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, _jQuery) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    // Declare widget's prototype.
+    return declare("ScrollThenFix.widget.ScrollThenFix", [_WidgetBase], {
+        // Parameters configured in the Modeler.
+        wrapClass: "",
+        affixTopClass: "",
+        offsetFromTop: 100,
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {},
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            console.log(this.id + ".postCreate");
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            console.log(this.id + ".update");
+
+            this._updateRendering();
+
+            callback();
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+        enable: function () {},
+
+        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+        disable: function () {},
+
+        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {},
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function () {
+            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+        },
+
+        // Rerender the interface.
+        _updateRendering: function () {
+
+            var wrap = $(this.wrapClass);
+            var toggledClass = this.toggledClass;
+            var offsetValue = this.offsetFromTop;
+
+            wrap.on("scroll", this._debounce(function (e) {
+
+                if (this.scrollTop > offsetValue) {
+                    wrap.addClass(toggledClass);
+                } else {
+                    wrap.removeClass(toggledClass);
+                }
+
+            }, 250));
+        },
+
+        // Returns a function, that, as long as it continues to be invoked, will not
+        // be triggered. The function will be called after it stops being called for
+        // N milliseconds. If `immediate` is passed, trigger the function on the
+        // leading edge, instead of the trailing.
+        _debounce: function (func, wait, immediate) {
+            var timeout;
+            return function () {
+                var context = this,
+                    args = arguments;
+                var later = function () {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                var callNow = immediate && !timeout;
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+                if (callNow) func.apply(context, args);
+            };
+        }
+    });
+});
+
+require(["ScrollThenFix/widget/ScrollThenFix"], function () {
+    "use strict";
+});
